8b04c224005ef7cbdba6229b1f05fa0eb9292fa4
matthewgorbett <Idontknow8112@@@>
2022-11-19 07:40:03 -0700

    vae

--- measure_lid.py
+++ measure_lid.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-dataset='kaggle_nid'
+dataset='unsw_nb15'
 directory='/s/luffy/b/nobackup/mgorb/iot/'
 #directory='csv/'
 if dataset=='ton_iot':
