1ebdfd9e45aba4c27df79f672d42c57f2005118f
matthewgorbett <Idontknow8112@@@>
2022-11-20 19:26:59 -0700

    vae

--- vae_generate.py
+++ vae_generate.py
@@ -365,6 +365,7 @@ def test(best_loss ):
             a = trainset_cont[torch.randperm(trainset_cont.size()[0])][:10]
             print(a.size())
             print(torch.cdist(da[:,:].float().cpu(),a.float()))
+            print(torch.cdist(da[:,:].float().cpu(),a.float()).size())
             sys.exit()
             output=None
             for cat in cat_outs:
